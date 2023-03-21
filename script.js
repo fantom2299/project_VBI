087d0b744b5ca35a15f5c4e6be47050cadbb26ee
nik <fantom2299@gmail.com>
2023-03-21 18:33:38 +0300

    project-2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // чекбокс на четные блоки
 document.getElementById('privacy_checkbox').addEventListener('click', function (ev) {
-    document.querySelector('.block_2, .block_4').style.background = (this.checked) ? '#A4A3A4' : ''
+    document.querySelector('.block_2').style.background = (this.checked) ? '#A4A3A4' : ''
 });
 
 document.getElementById('privacy_checkbox').addEventListener('click', function (ev) {
@@ -16,7 +16,37 @@ document.getElementById('privacy_checkbox').addEventListener('click', function (
     document.querySelector('.text_4').style.color = (this.checked) ? 'white' : ''
 });
 
+document.getElementById('privacy_checkbox').addEventListener('click', function (ev) {
+    document.querySelector('.block_6').style.background = (this.checked) ? '#A4A3A4' : ''
+});
+
+document.getElementById('privacy_checkbox').addEventListener('click', function (ev) {
+    document.querySelector('.text_6').style.color = (this.checked) ? 'white' : ''
+});
+
+
+document.getElementById('privacy_checkbox').addEventListener('click', function (ev) {
+    document.querySelector('.block_8').style.background = (this.checked) ? '#A4A3A4' : ''
+});
+
+document.getElementById('privacy_checkbox').addEventListener('click', function (ev) {
+    document.querySelector('.text_8').style.color = (this.checked) ? 'white' : ''
+});
+
+
+document.getElementById('privacy_checkbox').addEventListener('click', function (ev) {
+    document.querySelector('.block_10').style.background = (this.checked) ? '#A4A3A4' : ''
+});
+
+document.getElementById('privacy_checkbox').addEventListener('click', function (ev) {
+    document.querySelector('.text_10').style.color = (this.checked) ? 'white' : ''
+});
+
+
+
+
+
 //  событие на кнопку добавлет и убирает
 $('.btn').click(function () {
-    $(".block_5").fadeToggle(100);
+    $(".block_5, .block_6, .block_7, .block_8, .block_9, .block_10").fadeToggle(100);
 }); 
